97bb087ba63fdabc435f56ecb2e78d255f290114
Pat Chambers <z5264081@cse.unsw.edu.au>
2024-02-13 16:22:54 +1100

    Added code files

---
+++ week01/face.c
@@ -0,0 +1,14 @@
+// Printing out a smiley face in C
+// Pat Chambers (z5264081), 13/2/24
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o \n");
+    printf("\\_/\n");
+
+    return 0;
+}
